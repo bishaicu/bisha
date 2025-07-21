6722e402d95a2a08a63e23bc730e9524fdbfd543
bishaicu <medotonoby@gmail.com>
2025-07-21 22:55:51 +0300

    Add files via upload
    Signed-off-by: bishaicu <medotonoby@gmail.com>

---
+++ 00_Login.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import os
+
+st.set_page_config(page_title="ICU Login", layout="centered")
+
+st.title("🔐 ICU Dashboard Login")
+
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
+
+if st.button("Login"):
+    if username == "admin" and password == "admin123":
+        st.success("Login successful! Welcome, admin.")
+        st.switch_page("pages/Data_Entry")
+    else:
+        st.error("Invalid credentials")
---
+++ app.py
@@ -0,0 +1,8 @@
+import os
+os.environ["STREAMLIT_DISABLE_USAGE_STATS"] = "1"
+os.environ["STREAMLIT_HOME"] = "/tmp"
+
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+
+switch_page("00_Login")
