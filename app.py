1eaf136ce7593237affb694e57977af84316ef71
bishaicu <medotonoby@gmail.com>
2025-07-22 08:32:12 +0300

    Update app.py
    Signed-off-by: bishaicu <medotonoby@gmail.com>

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ os.environ["STREAMLIT_DISABLE_USAGE_STATS"] = "1"
 os.environ["STREAMLIT_HOME"] = "/tmp"
 
 import streamlit as st
-from streamlit_extras.switch_page_button import switch_page
+from streamlit_extras.switch_page_button 
+import switch_page
 
 switch_page("00_Login")
